c439a225479b87f89058a710337ef5c8440922bb
peter lee <peter@peters-MacBook-Pro.local>
2025-03-05 23:01:16 +0800

    lab6

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -4,23 +4,23 @@ import java.io.File;
 import static capers.Utils.*;
 
 /** A repository for Capers 
- * @author TODO
+ * @author Peter
  * The structure of a Capers Repository is as follows:
  *
  * .capers/ -- top level folder for all persistent data in your lab12 folder
  *    - dogs/ -- folder containing all of the persistent data for dogs
  *    - story -- file containing the current story
  *
- * TODO: change the above structure if you do something different.
+ *
  */
 public class CapersRepository {
     /** Current Working Directory. */
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
-
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers");
+    static final File DOGS_FOLDER = Utils.join(CAPERS_FOLDER,"dogs");
+    static final File STORY_FOLDER = Utils.join(CAPERS_FOLDER,"story");
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
@@ -31,7 +31,9 @@ public class CapersRepository {
      *    - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        CAPERS_FOLDER.mkdir();
+        DOGS_FOLDER.mkdir();
+        STORY_FOLDER.mkdir();
     }
 
     /**
@@ -40,7 +42,16 @@ public class CapersRepository {
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File storyFile = new File(STORY_FOLDER, "story");
+        if (!storyFile.exists()) {
+            Utils.writeContents(storyFile, text);
+            System.out.println(text);
+        }
+        else {
+            String file = Utils.readContentsAsString(storyFile);
+            Utils.writeContents(storyFile,file + "\n" + text);
+            System.out.println(file + "\n" + text);
+        }
     }
 
     /**
@@ -49,7 +60,9 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog new_dog = new Dog(name,breed,age);
+        new_dog.saveDog();
+        System.out.println(new_dog.toString());
     }
 
     /**
@@ -59,6 +72,8 @@ public class CapersRepository {
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+           Dog dog = Dog.fromFile(name);
+           dog.haveBirthday();
+           dog.saveDog();
     }
 }
--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -2,16 +2,15 @@ package capers;
 
 import java.io.File;
 import java.io.Serializable;
-import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author Peter
 */
-public class Dog { // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // TODO (hint: look at the `join`
-                                         //      function in Utils)
+    static final File DOG_FOLDER = Utils.join(System.getProperty("user.dir"),".capers", "dogs");
+    //      function in Utils)
 
     /** Age of dog. */
     private int age;
@@ -39,8 +38,9 @@ public class Dog { // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        // TODO (hint: look at the Utils file)
-        return null;
+        File dogFile = new File(DOG_FOLDER, name);
+        Dog d = Utils.readObject(dogFile , Dog.class);
+        return d;
     }
 
     /**
@@ -56,7 +56,9 @@ public class Dog { // TODO
      * Saves a dog to a file for future use.
      */
     public void saveDog() {
-        // TODO (hint: don't forget dog names are unique)
+        File dogFile = new File(DOG_FOLDER, this.name);
+        Utils.writeObject(dogFile, this);
+
     }
 
     @Override
